b5d37dbc5c602be261814c704092e102be3bc899
Vitoria Rodrigues <vvitoria.rodrigues12@gmail.com>
2023-03-31 10:40:56 +0100

    Add Dados

---
+++ RandomDice/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace RandomDice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Numero de dados a lançar:");
+            string r=Console.ReadLine();
+            int n=Int32.Parse(r);
+            Random rand= new Random();
+            Console.WriteLine($"Os Dados:");
+
+            for (int i=0; i<n; i++)
+            {
+               Console.WriteLine($"{rand.Next(1,6)}"); 
+            }
+        }
+    }
+}
