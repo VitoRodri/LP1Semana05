b2921163a94bd409407e80d5a93c2e18495620fd
Vitoria Rodrigues <vvitoria.rodrigues12@gmail.com>
2023-03-31 11:11:17 +0100

    Add Uppercase

---
+++ UpperUnion/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text;
+
+namespace UpperUnion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (string strings in args)
+            {
+                Console.WriteLine($"{strings.ToUpper()}");
+            }
+            
+
+        }
+    }
+}
