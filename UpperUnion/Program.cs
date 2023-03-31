db4c4dd1f7676f2a5fc51665cff15e6838b45441
Vitoria Rodrigues <vvitoria.rodrigues12@gmail.com>
2023-03-31 11:37:27 +0100

    Add alphabetical order

--- UpperUnion/Program.cs
+++ UpperUnion/Program.cs
@@ -1,17 +1,24 @@
 ﻿using System;
 using System.Text;
 
+
 namespace UpperUnion
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Array.Sort(args);
+
             foreach (string strings in args)
             {
                 Console.WriteLine($"{strings.ToUpper()}");
             }
+    
             
+            
+            
+        
 
         }
     }
